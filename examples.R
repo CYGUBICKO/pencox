fdfbde0df16adbc43f535d48d31a85c4b933316e
cygubicko <cygu@aims.ac.za>
2020-03-20 11:53:02 -0400

    Autocommit

--- examples.R
+++ examples.R
@@ -25,7 +25,7 @@ X <- (df
 	%>% select(-c("trt", "time", "status"))
 	%>% data.frame()
 )
-pencox_res <- pencox(eventvar, X, gamma = 0.1, alpha = 1, lambda = 0, maxiter = 500, standardise = TRUE)
+pencox_res <- pencox(eventvar, X, gamma = 0.1, alpha = 0.3, lambda = 0, maxiter = 800, standardise = TRUE)
 pencox_res
 
 ## glmnet
@@ -33,7 +33,7 @@ timevar <- df$time
 y <- Surv(time = timevar, event = eventvar)
 formula <- as.formula(paste0("~", colnames(X), collapse = "+"))
 X2 <- scale(model.matrix(formula, X)[,-1])
-glmnet_res <- coef(glmnet(x = X2 ,y = y, family = 'cox', alpha = 1, lambda = 0,standardize = FALSE))
+glmnet_res <- coef(glmnet(x = X2 ,y = y, family = 'cox', alpha = 0.3, lambda = 0,standardize = FALSE))
 glmnet_res
 
 ## Cox-PH
@@ -43,7 +43,7 @@ d1 <- (X2
 )
 coxph_res <- coxph(Surv(time = timevar, event = eventvar) ~., data = d1)
 summary(coxph_res)
-
+quit()
 ## Compare speed
 speed_test <- microbenchmark(
 	"pencox" = {pencox(eventvar, X, gamma = 0.1, alpha = 1, lambda = 1, maxiter = 200, standardise = TRUE)}
